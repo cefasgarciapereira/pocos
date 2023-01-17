f94c80fa36d7e3fbbd54c8491491541e3cf28c21
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2023-01-17 18:20:09 -0300

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,109 +14,60 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+      <main>
+        <h1 className={styles.title}>Poços de Caldas</h1>
+        <p className={styles.description}>
+          Sobre a cidade, pessoas que habitam e memórias de uma sociedade.
+        </p>
+        <img
+          alt="Telephone Booths, 1967"
+          src="https://www.museothyssen.org/webdocs/conectathyssen/cartas-amor-ciudades/wp-content/uploads/2022/07/1977-5.jpg"
+          width={"100%"}
+        />
+        <p>
+          Richard Estes
+          <br />
+          Telephone Booths, 1967
+          <br />
+          Acrylic on masonite. 122 x 175.3 cm
+          <br />
+          Museo Nacional Thyssen-Bornemisza, Madrid
+        </p>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
+        <div className={styles.gallery}>
+          <img
+            alt="Telephone Booths, 1967"
+            src="https://www.museothyssen.org/webdocs/conectathyssen/cartas-amor-ciudades/wp-content/uploads/2022/07/1977-5.jpg"
+            width={"100%"}
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+          <img
+            alt="Telephone Booths, 1967"
+            src="https://www.museothyssen.org/webdocs/conectathyssen/cartas-amor-ciudades/wp-content/uploads/2022/07/1977-5.jpg"
+            width={"100%"}
+          />
+          <img
+            alt="Telephone Booths, 1967"
+            src="https://www.museothyssen.org/webdocs/conectathyssen/cartas-amor-ciudades/wp-content/uploads/2022/07/1977-5.jpg"
+            width={"100%"}
+          />
+          <img
+            alt="Telephone Booths, 1967"
+            src="https://www.museothyssen.org/webdocs/conectathyssen/cartas-amor-ciudades/wp-content/uploads/2022/07/1977-5.jpg"
+            width={"100%"}
+          />
+          <img
+            alt="Telephone Booths, 1967"
+            src="https://www.museothyssen.org/webdocs/conectathyssen/cartas-amor-ciudades/wp-content/uploads/2022/07/1977-5.jpg"
+            width={"100%"}
+          />
+
         </div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        <blockquote className={styles.subtitle}>
+          "A cidade é uma combinação de muitas coisas: memória, desejos, signos de uma linguagem; é um lugar de troca, como qualquer
+          livro de história econômica lhe dirá - só que essas trocas não são apenas trocas de mercadorias, mas também envolvem palavras,
+          desejos e memórias. <br />Italo Calvino
+        </blockquote>
       </main>
     </>
   )
